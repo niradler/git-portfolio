2e882f7d7b29a5e354404a240be15feb9dafa7f2
Nir Adler <niradler55@gmail.com>
2019-05-27 23:15:28 +0300

    v0.0.1

--- readme.md
+++ readme.md
@@ -1,3 +1,14 @@
-# git portfolio
+# protfolio
+
+Generate portfolio website from git service such as github.
+
+# Usage
+
+```
+mkdir portfolio
+cd portfolio
+npx protfolio gen -u <github username>
+```
+
 
 [inspired by gitfolio](https://github.com/imfunniee/gitfolio)
--- tests/generate.test.js
+++ tests/generate.test.js
@@ -1,12 +1,17 @@
 const { generate } = require("../src");
 const fs = require("file-system");
 
-test("generate", () => {
+test("generate", async () => {
   fs.rmdirSync("./output");
-  generate({
+  const templateData = await generate({
     exportPath: "./output",
     username: "niradler",
     useTestData: true
+  }).catch(e => {
+    throw e;
   });
   expect(fs.readFileSync("./output/index.html", "utf-8")).toBeDefined();
+  expect(templateData.config).toBeDefined();
+  expect(templateData.repositories).toBeDefined();
+  expect(templateData.user).toBeDefined();
 });
